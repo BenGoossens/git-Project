307ca2cb619c6a9c9dbabd26353dc72a5a447565
BenGoossens <ben.goossens.u9178@student.hogent.be>
2015-02-12 17:07:21 +0100

    klassendiagram aangemaakt (niet volledig, test om commit te proberen)

---
+++ Project_Krekelhof/Verteltas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace Project_Krekelhof
+{
+    public class Verteltas : Item
+    {
+    }
+}
