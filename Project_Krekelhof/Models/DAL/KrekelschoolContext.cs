9e41693422d9c6ba544f492a81fc091fbd1b3b54
BenGoossens <ben.goossens.u9178@student.hogent.be>
2015-02-19 12:27:43 +0100

    testProject en context aangemaakt

---
+++ Project_Krekelhof/Models/DAL/KrekelschoolContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace Project_Krekelhof.Models.DAL
+{
+    public class KrekelschoolContext : DbContext
+    {
+        public KrekelschoolContext() : base("krekelschool")
+        {
+            
+        }
+
+        public DbSet<IItem> items { get; set; }
+        public DbSet<Leerling> leerlingen { get; set; }
+        public DbSet<Uitlening> uitleningen { get; set; }
+        public DbSet<Categorie> categorieen { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.AddFromAssembly(Assembly.GetExecutingAssembly());
+
+        }
+    }
+}
