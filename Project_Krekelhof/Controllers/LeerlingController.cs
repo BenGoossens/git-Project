f83a2c1c97a1fb1c9f945eaef2502e20b918ba46
BenGoossens <ben.goossens.u9178@student.hogent.be>
2015-02-19 10:32:48 +0100

    controllers zijn toegevoegd

---
+++ Project_Krekelhof/Controllers/LeerlingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_Krekelhof.Controllers
+{
+    public class LeerlingController : Controller
+    {
+        // GET: Leerling
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
