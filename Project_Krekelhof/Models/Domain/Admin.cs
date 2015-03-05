edc1dc229b88dfaa61f50ab96a22ba7e98fad7e0
BenGoossens <ben.goossens.u9178@student.hogent.be>
2015-03-05 12:07:39 +0100

    Domein verder aangvuld

---
+++ Project_Krekelhof/Models/Domain/Admin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_Krekelhof.Models.Domain
+{
+    public class Admin : Medewerker
+    {
+        public virtual ICollection<Item> Items { get; set; }
+        public virtual ICollection<Leerling> Leerlingen { get; set; }
+        public Item ItemToevoegen()
+        {
+            //code
+            return null;
+        }
+
+        public Item ItemAanpassen()
+        {
+            //code
+            return null;
+        }
+
+        public void ItemVerwijderen(Item item)
+        {
+            if (!Items.Contains(item))
+                throw new ArgumentException(string.Format("{0} bestaat niet", item.Naam));
+            Items.Remove(item);
+        }
+
+        public Leerling LeerlingToevoegen()
+        {
+            //code
+            return null;
+        }
+
+        public Leerling LeerlingAanpassen()
+        {
+            //code
+            return null;
+        }
+
+        public void LeerlingVerwijderen(Leerling leerling)
+        {
+            if (!Leerlingen.Contains(leerling))
+                throw new ArgumentException(string.Format("{0} {0} bestaat niet", leerling.Voornaam));
+            Leerlingen.Remove(leerling);
+        }
+    }
+}
---
+++ Project_Krekelhof/Models/Domain/Medewerker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_Krekelhof.Models.Domain
+{
+    public class Medewerker  :Gebruiker
+    {
+        public virtual ICollection<Uitlening> Uitleningen { get; set; }
+        public Uitlening UitleningToevoegen(int id, DateTime startUitlening, DateTime eindeUitlening, bool isTerug)
+        {
+            //code
+            return null;
+        }
+
+        public Uitlening UitleningAanpassen()
+        {
+            //code
+            return null;
+        }
+
+        public void UitleningVerwijderen(Uitlening uitlening)
+        {
+            if (!Uitleningen.Contains(uitlening))
+                throw new ArgumentException(string.Format("{0} bestaat niet!", uitlening.Id));
+            Uitleningen.Remove(uitlening);
+        }
+    }
+}
