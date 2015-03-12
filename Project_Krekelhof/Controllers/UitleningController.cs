21419318d7953f1b4512427d10cb79cfa496e27a
kaotto <kasper_ottoy2@hotmail.com>
2015-03-12 09:48:29 +0100

    ModelView

---
+++ Project_Krekelhof/Controllers/UitleningController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project_Krekelhof.Models.Domain;
+using Project_Krekelhof.ViewModels;
+namespace Project_Krekelhof.Controllers
+{
+    public class UitleningController : Controller
+    {
+
+
+        private IUitleningRepository uitleningRepository;
+        private IItemRepository itemRepository;
+        private ILeerlingRepository leerlingRepository;
+
+        public UitleningController(IUitleningRepository uitleningRepository, IItemRepository itemRepository,ILeerlingRepository leerlingRepository)
+        {
+            this.uitleningRepository = uitleningRepository;
+            this.itemRepository = itemRepository;
+            this.leerlingRepository = leerlingRepository;
+        }
+
+        public ActionResult Index()
+        {
+            //Ophalen uitleningen
+            IEnumerable<Uitlening> uitleningen = 
+                uitleningRepository.FindAll();
+
+            //Aanmaken van ViewModel.  ToList zorgt voor het uitvoeren van de query
+
+            IEnumerable<UitleningIndexViewModel> vms =
+               uitleningen.Select(u => new UitleningIndexViewModel(u)).ToList();            
+               return View(vms);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//private IUitleningenRepository repos;
+
+//        public UitleningController(KrekelSchoolContext context)
+//        {
+//            repos = new UitleningRepository(context);
+//        }
+//        public Collection<Uitlening> Uitleningen
+//        {
+//            get
+//            {
+//                throw new System.NotImplementedException();
+//            }
+//            set
+//            {
+//            }
+//        }
+
+//        public void EditUitlening(Uitlening uitlening)
+//        {
+//            // Remove add = edit?
+//            // removeUitlening(uitlening);
+           
+//        }
+
+//        public void RemoveUitlening(Uitlening uitlening)
+//        {
+//            repos.Delete(uitlening);
+//        }
+
+//        public Uitlening GetUitlening()
+//        {
+//            throw new System.NotImplementedException();
+//        }
+
+//        public Collection<Uitlening> GetUitleningen()
+//        {
+//            throw new System.NotImplementedException();
+//        }
+
+//        public void CheckOutUitlening(Uitlening uitlening)
+//        {
+//            // uitleningeinddatum < huidigeDatum => Boete 
+//            // schade Claim => Boete (laag, hoge claim)
+//            // Item schade op geclaimde schade.
+//            // UitleningeindDatum > huidige datum => No problem check that shit out
+//            // beschikbaar van item op true
+//            // 
+//            throw new System.NotImplementedException();
+//        }
+
+//        public void AddUitlening(Lener leerling, DateTime uitlendatum, Collection<Item> items)
+//        {
+//            //Kinderboeken 1 week , andere 2 weken (Kijken op leeftijd)
+//            //item beschikbaar false 
+            
+//            throw new System.NotImplementedException();
+//        }
---
+++ Project_Krekelhof/ViewModels/UitleningViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Project_Krekelhof.Models.Domain;
+
+namespace Project_Krekelhof.ViewModels
+{
+    public class UitleningViewModel
+    {
+        public UitleningViewModel()
+        {
+            
+        }
+        public UitleningViewModel(Uitlening u)
+        {
+            Id = u.Id;
+            EindeUitlening = u.EindeUitlening;
+            IsTerug = u.IsTerug;
+            Item = u.Item;
+            StartUitlening = u.StartUitlening;
+            
+            
+        }
+        public int Id { get;  set; }
+        public DateTime EindeUitlening { get;  set; }
+
+        public DateTime StartUitlening { get;  set; }
+
+        public  bool IsTerug {get;set;}
+        
+        public  Item Item {get;set;}       
+
+    }
+
+    public class UitleningIndexViewModel
+    {
+        public UitleningIndexViewModel(Uitlening u)
+        {
+            Id = u.Id;
+            EindeUitlening = u.EindeUitlening;
+            IsTerug = u.IsTerug;
+            Item = u.Item;
+            StartUitlening = u.StartUitlening;
+
+
+        }
+        public int Id { get; set; }
+        public DateTime EindeUitlening { get; set; }
+
+        public DateTime StartUitlening { get; set; }
+
+        public bool IsTerug { get; set; }
+
+        public Item Item { get; set; }     
+    }
+
+
+
+    }
